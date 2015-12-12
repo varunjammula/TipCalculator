d41b22b4af05b832e474aa1a06168216c52b2aa6
vjammula <varunjammula@gmail.com>
2015-12-12 11:28:23 -0700

    Added project files

---
+++ SettingsViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Varun Chandra Jammula on 12/12/15.
+//  Copyright © 2015 Varun Chandra Jammula. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var defaultTip: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let tipIndex = defaults.integerForKey("defaultTipIndex")
+        defaultTip.selectedSegmentIndex = tipIndex
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func saveDefaultTip(sender: AnyObject) {
+        let dTips = [18, 20, 25]
+        let dTipIndex = defaultTip.selectedSegmentIndex
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(dTipIndex, forKey: "defaultTipIndex")
+        defaults.synchronize()
+        // create the alert
+        let alert = UIAlertController(title: "Tipper", message: "Default tip set to: " + String(dTips[dTipIndex])+"%", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        // add an action (button)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        
+        // show the alert
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+
+    @IBAction func hideKeyboard(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tips/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Varun Chandra Jammula on 12/11/15.
+//  Copyright © 2015 Varun Chandra Jammula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipSelector: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let tipIndex = defaults.integerForKey("defaultTipIndex")
+        tipSelector.selectedSegmentIndex = tipIndex
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        let billAmount = (billField.text! as NSString).doubleValue
+        let tipPercentages = [0.18, 0.2, 0.25]
+        let tipPercentage = tipPercentages[tipSelector.selectedSegmentIndex]
+        let tip = billAmount * tipPercentage
+        let totalAmount = billAmount + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", totalAmount)
+        
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
+
